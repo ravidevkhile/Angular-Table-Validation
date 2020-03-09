baa931f05e5415268d7a6642bd5440f51cc6feb0
unknown <ravidevkhile@gmail.com>
2020-03-09 08:32:20 +0530

    Botsrap Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableFormComponent } from './table-form/table-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/table-form/table-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-form',
+  templateUrl: './table-form.component.html',
+  styleUrls: ['./table-form.component.css']
+})
+export class TableFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
