2c6f0dbacd54902a0ac09703be0ac2c71d967893
unknown <ravidevkhile@gmail.com>
2020-03-09 10:35:07 +0530

    Table Formt Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TableFormComponent } from './table-form/table-form.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     TableFormComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/table-form/table-form.component.ts
+++ src/app/table-form/table-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-table-form',
@@ -6,10 +7,76 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table-form.component.css']
 })
 export class TableFormComponent implements OnInit {
-
-  constructor() { }
+  userForm: FormGroup;
+  tableHeader:TableHeader[];
+  const formControls=[];  
+  constructor(private fb: FormBuilder) {
+    this.tableHeader=[{
+      fieldName:'name',
+      header:'Name',
+      validator:['required']
+    },{
+      fieldName:'email',
+      header:'Email',
+      validator:['required','emailValidator',]
+    },
+    {
+      fieldName:'dateFrom',
+      header:'From Date',
+      validator:['required']
+    },{
+      fieldName:'dateTo',
+      header:'To Date',
+      validator:['required,dateLessThan("dateTo","dateFrom")']
+    },{
+      fieldName:'mobNumber',
+      header:'Mobile Number',
+      validator:['required,numberValidator())']
+    }]
+   }
 
   ngOnInit(): void {
+    // this.userForm = this.fb.group({      
+    //   users: this.fb.array([])
+    // });
+    
+    this.userForm = this.fb.group({      
+      users: this.fb.array([])
+    });
   }
-
+  initiatForm(): FormGroup {
+    return this.fb.group({
+      name: ['',],
+      email: ['',],
+      dateTo: ['',],
+      dateFrom: [''],
+      mobNumber: ['']
+    });
+  }
+  numberValidator(c: AbstractControl): { [key: string]: boolean } | null {
+    if (c.pristine) {
+      return null;
+    }
+    if (c.value.match(/.*[^0-9].*/)) {
+      return { 'numeric': true };
+    }
+    return null;
+  }
+  dateLessThan(from: string, to: string) {
+    return (group: FormGroup): { [key: string]: any } => {
+      let f = group.controls[from];
+      let t = group.controls[to];
+      if (f.value > t.value) {
+        return {
+          dates: "Date from should be less than Date to"
+        };
+      }
+      return {};
+    }
+  }
+}
+export class TableHeader{
+fieldName:string;
+header:string;
+validator:string[];
 }
